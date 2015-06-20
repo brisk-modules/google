2741158920b928a3500e676f8602891dce4191cd
Makis Tracend <makis.tracend@gmail.com>
2015-06-20 02:24:25 -0700

    + api controller

---
+++ app/controllers/api/google.js
@@ -0,0 +1,41 @@
+var brisk = require('brisk'),
+	Parent = brisk.getBaseController("data");
+
+/*
+controller = Parent.extend({
+
+	index: function(req, res){
+		//
+		this.rest(crud, req, res);
+	}
+
+});
+*/
+
+var crud = {
+
+	create: function(req, res){
+
+	},
+
+	read: function(req, res){
+
+	},
+
+	update: function(req, res){
+
+	},
+
+	del: function(req, res){
+
+	},
+
+
+}
+
+// Helpers
+
+//...
+
+
+module.exports = crud;
